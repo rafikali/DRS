be8f63e586a431432a0d99cc6db473b176098456
rafik ali <ra3210304@gmail.com>
2022-03-09 01:04:18 +0545

    appbarshaper'

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,6 +1,9 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:login_page/widgets/login_textfield.dart';
+
+
 
 
 class RegisterPage extends StatefulWidget {
@@ -14,41 +17,81 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-    appBar: AppBar(
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(
-          bottom: Radius.circular(10)
-        )
-      ),
-    ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children:  [
-            ListTile(
-              title: Row(
-                children: const [
-                  Icon(CupertinoIcons.profile_circled),
-                  Expanded(
-                    child: LoginTextForm(
-                        hintText: ' username', labelText: 'Username'),
-                  ),
-
-                ],
-              )
-
+    return  Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(200.0),
+        child: AppBar(
+          backgroundColor: Colors.indigo[600],
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.elliptical(150, 35),
             )
+          ),
 
-            // ListTile(
-            //   leading: Icon(CupertinoIcons.profile_circled,
-            //   size: 30,),
-            //   title: SimpleTextField(),
-            //
-            // )
-          ],
         ),
+
       ),
+      body: ListView(
+        children:  [
+         ListTile(
+           leading: Icon(CupertinoIcons.profile_circled,
+           color: Colors.blue,
+           size: 40,),
+           contentPadding: EdgeInsets.zero,
+           title: LoginTextForm(hintText: 'hintText', labelText: 'hey'),
+
+         )
+
+        ],
+      ),
+      // body: NestedScrollView(
+      //   headerSliverBuilder: (BuildContext context, bool) {
+      //     return <Widget> [
+      //        SliverAppBar(
+      //         
+      //         backgroundColor: Colors.indigo[600],
+      //         expandedHeight: 150,
+      //         floating: true,
+      //         pinned: true,
+      //         snap: true,
+      //       )
+      //     ];
+      //   },
+      //   body:Padding(
+      //     padding: const EdgeInsets.all(10.0),
+      //     child: Column(
+      //         children:  [
+      //     ListTile(
+      //     title: Row(
+      //     children: const [
+      //         Icon(CupertinoIcons.profile_circled),
+      //     Expanded(
+      //       child: LoginTextForm(
+      //           hintText: ' username', labelText: 'Username'),
+      //     ),
+      //
+      //     ],
+      //   )
+      //
+      // ) ,
+      // ]
+      // ),
+    // appBar: AppBar(
+    //   shape: const RoundedRectangleBorder(
+    //
+    //     borderRadius: BorderRadius.vertical(
+    //       bottom: Radius.circular(10)
+    //     )
+    //   ),
+    // ),
+
+      // body:
+
+
+
+        
+        
+
 
     );
   }
